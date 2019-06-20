c8d00083d3d47fdff16890fcc23944bd4df0f69a
LU <yiying.lu@outlook.com>
2019-06-20 18:18:08 +0200

    Vérification des données saisies (au secours ça marche pas !)

--- formPlat.php
+++ formPlat.php
@@ -1,6 +1,64 @@
 <?php
+$erreur = " ";
+$success = " ";
 include_once('connexionBDD.php');
+if(isset($_POST['formPlat']))
+{
+    //Récupérer les données
+    if (!empty($_POST["nomPlat"] && $_POST["urlImage"] && $_POST["description"] && $_POST["quantiteDispo"] && $_POST["prixUnitaire"]))
+    {
+          $nomPlat = $_POST["nomPlat"];
+          $urlImage = $_POST["urlImage"];
+          $description = $_POST["description"];
+          $quantiteDispo = $_POST["quantiteDispo"];
+          $prixUnitaire = $_POST["prixUnitaire"];
+
+          //verification du nombre de caractère saisie
+          $nomPlatLength=strlen($nomPlat);
+          if($nomPlatLength <=150)
+          {
+              $urlImageLength=strlen($urlImage);
+              if($urlImageLength<=150)
+              {
+                  $descriptionLength=strlen($description);
+                  if($descriptionLength<=150)
+                  {
+                      $query = "INSERT INTO Plat(nomPlat, urlImage, description, quantiteDispo, prixUnitaire) VALUES ('$nomPlat', '$urlImage', '$quantiteDispo', '$prixUnitaire');";
+                      $result = mysqli_query($mysqli,$query);
+                      if($result)
+                      {
+                          while($nomPlat = mysqli_fetch_assoc($result))
+                          {
+                              $success ='<p style="text-align:center; color:green"> Votre plat'.$nomPlat['nomPlat'].' a été bien ajouté !</p>';
+                          }
+                      }
+                      $mysqli->close();
+                  }
+                  else
+                  {
+                      $erreur='<p style="text-align:center; color:red">Votre description dépasse 150 caractères!<p>';
+                  }
+
+              }
+              else
+              {
+                  $erreur='<p style="text-align:center; color:red">Votre urlImage dépasse 150 caractères!<p>';
+              }
+
+          }
+          else
+          {
+              $erreur='<p style="text-align:center; color:red">Le nom du plat dépasse 150 caractères !<p>';
+          }
+    }
+    else
+    {
+         $erreur='<p style="text-align:center; color:red">L\'un des champs n\'a pas été complété !<p>';
+    }
+}
+
 ?>
+
 <!DOCTYPE html>
 <html>
  <head>
@@ -17,8 +75,11 @@ include_once('connexionBDD.php');
     </style>
  </head>
 <body>
-    <h1> Formulaire Plats </h1>
-
+    <h1> Formulaire Plat </h1>
+    <?php
+    if (isset($erreur)) { echo $erreur; }
+    if (isset($success)) { echo $success; }
+    ?>
     <form name="formPlat" action="" method="POST">
 
                 <label>Nom</label>
@@ -30,7 +91,7 @@ include_once('connexionBDD.php');
                 <br/>
 
                <label>Description du plat </label>
-                <input type="text" name="" size="20" maxlength="80"   />
+                <input type="text" name="description" size="20" maxlength="80"   />
                 <br/>
 
                 <label>Quantité disponible </label>
@@ -38,7 +99,7 @@ include_once('connexionBDD.php');
                  <br/>
 
                 <label>prix unitaire </label>
-                 <input type="text" name="prixunitaire" size="20" maxlength="80"   />
+                 <input type="text" name="prixUnitaire" size="20" maxlength="80"   />
                  <br/>
 
                 <input type="submit" value="Valider" class="bouton" />
