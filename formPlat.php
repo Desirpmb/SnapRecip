3c13340d3ab3af2c52d2f4a637566e494c9a8e46
LU <yiying.lu@outlook.com>
2019-06-20 14:08:01 +0200

    Formulaire ajout/modif plat

---
+++ formPlat.php
@@ -0,0 +1,47 @@
+<?php
+include_once('connexionBDD.php');
+?>
+<!DOCTYPE html>
+<html>
+ <head>
+     <meta charset="UTF-8" />
+     <meta name="author" content="Manuele" />
+    <title> Formulaire</title>
+    <style type="text/css">
+          label { display: inline-block;
+                     width : 200px;
+                     text-align : left;
+                     font-weight: bold;
+                     margin-left: 5px;
+                    }
+    </style>
+ </head>
+<body>
+    <h1> Formulaire Plat </h1>
+
+    <form name="formPlat" action="" method="POST">
+
+                <label>Nom</label>
+                <input type="text" name="nomPlat" size="20" maxlength="80" />
+                <br/>
+
+                <label>Image</label>
+                <input type="text" name="urlImage" size="20" maxlength="80"   />
+                <br/>
+
+               <label>Description du plat </label>
+                <input type="text" name="" size="20" maxlength="80"   />
+                <br/>
+
+                <label>Quantité disponible </label>
+                 <input type="text" name="quantiteDispo" size="20" maxlength="80"   />
+                 <br/>
+
+                <label>prix unitaire </label>
+                 <input type="text" name="prixunitaire" size="20" maxlength="80"   />
+                 <br/>
+
+                <input type="submit" value="Valider" class="bouton" />
+    </form>
+</body>
+</html>
