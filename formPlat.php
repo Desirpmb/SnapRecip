1af3884eaba425450e57e3ef6ddecd27d7d13dac
LU <yiying.lu@outlook.com>
2019-06-20 21:04:46 +0200

    ça marche toujours pas (problème de Insert à la bdd)

--- formPlat.php
+++ formPlat.php
@@ -7,11 +7,12 @@ if(isset($_POST['formPlat']))
     //Récupérer les données
     if (!empty($_POST["nomPlat"] && $_POST["urlImage"] && $_POST["description"] && $_POST["quantiteDispo"] && $_POST["prixUnitaire"]))
     {
-          $nomPlat = $_POST["nomPlat"];
-          $urlImage = $_POST["urlImage"];
+          $nomPlat = htmlspecialchars($_POST["nomPlat"]);
+          $urlImage = htmlspecialchars($_POST["urlImage"]);
           $description = $_POST["description"];
           $quantiteDispo = $_POST["quantiteDispo"];
           $prixUnitaire = $_POST["prixUnitaire"];
+          $id = '\N'; /* auto-increment */
 
           //verification du nombre de caractère saisie
           $nomPlatLength=strlen($nomPlat);
@@ -21,22 +22,24 @@ if(isset($_POST['formPlat']))
               if($urlImageLength<=150)
               {
                   $descriptionLength=strlen($description);
-                  if($descriptionLength<=150)
+                  if($descriptionLength<=250)
                   {
-                      $query = "INSERT INTO Plat(nomPlat, urlImage, description, quantiteDispo, prixUnitaire) VALUES ('$nomPlat', '$urlImage', '$quantiteDispo', '$prixUnitaire');";
+                      $query = "INSERT INTO Plat(Id, nomPlat, urlImage, description, quantiteDispo, prixUnitaire) VALUES ('$id','$nomPlat','$urlImage','$description', '$quantiteDispo', '$prixUnitaire')";
                       $result = mysqli_query($mysqli,$query);
-                      if($result)
+                      if (!$result)
                       {
-                          while($nomPlat = mysqli_fetch_assoc($result))
-                          {
-                              $success ='<p style="text-align:center; color:green"> Votre plat'.$nomPlat['nomPlat'].' a été bien ajouté !</p>';
-                          }
+                          echo "<p>Désolée, requête impossible ! </p>";
+                      }
+                      else
+                      {
+                          $nomPlat = mysqli_fetch_assoc($result);
+                          $success ='<p style="text-align:center; color:green"> Votre plat'.$nomPlat['nomPlat'].' a été bien ajouté !</p>';
                       }
                       $mysqli->close();
                   }
                   else
                   {
-                      $erreur='<p style="text-align:center; color:red">Votre description dépasse 150 caractères!<p>';
+                      $erreur='<p style="text-align:center; color:red">Votre description dépasse 250 caractères!<p>';
                   }
 
               }
@@ -83,26 +86,27 @@ if(isset($_POST['formPlat']))
     <form name="formPlat" action="" method="POST">
 
                 <label>Nom</label>
-                <input type="text" name="nomPlat" size="20" maxlength="80" />
+                <input type="text" id="nomPlat" name="nomPlat" placeholder="Saisissez le nom de votre plat" size="40" maxlength="150" />
                 <br/>
 
                 <label>Image</label>
-                <input type="text" name="urlImage" size="20" maxlength="80"   />
+                <input type="text" id="urlImage" name="urlImage" placeholder="Saisissez l'URL de votre image" size="40" maxlength="150" />
                 <br/>
 
-               <label>Description du plat </label>
-                <input type="text" name="description" size="20" maxlength="80"   />
+                <label>Description du plat </label>
+                <input type="text" id="description" name="description" placeholder="Saisissez la description de votre plat" size="40" maxlength="250" />
                 <br/>
 
                 <label>Quantité disponible </label>
-                 <input type="text" name="quantiteDispo" size="20" maxlength="80"   />
-                 <br/>
+                <input type="text" id="quantiteDispo" name="quantiteDispo" placeholder="Saisissez la quantité disponible de votre plat" size="40" maxlength="5" />
+                <br/>
 
-                <label>prix unitaire </label>
-                 <input type="text" name="prixUnitaire" size="20" maxlength="80"   />
-                 <br/>
+                <label>Prix unitaire </label>
+                <input type="text" id="prixUnitaire" name="prixUnitaire" placeholder="Saisissez le prix unitaire de votre plat" size="40" maxlength="5" />
+                <br/>
 
-                <input type="submit" value="Valider" class="bouton" />
+                <input type="submit" value="Ajouter votre plat" class="bouton" />
+
     </form>
 </body>
 </html>
