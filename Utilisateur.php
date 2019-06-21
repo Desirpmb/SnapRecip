0558cfa44fdd458b0057a87aad8491cde1a3e905
desirpmb <desirpembele@hotmail.com>
2019-06-21 20:41:26 +0200

    creation de la class utilisateur

---
+++ Utilisateur.php
@@ -0,0 +1,18 @@
+<?php
+    class Utilisateur{
+        
+        private $nom;
+        private $prenom;
+        private $sexe;
+        private $dateNaissance;
+        private $email;
+        private $motDePasse;
+        private $isVendeur;
+
+        public function getNom(){
+            $
+                
+        
+        }
+    }
+?>
