3e2826878e66f110c67733f3d773982dfc4b4ca3
desirpmb <desirpembele@hotmail.com>
2019-06-22 20:26:19 +0200

    modif formPlat.php

--- formPlat.php
+++ formPlat.php
@@ -2,87 +2,110 @@
 $erreur = " ";
 $success = " ";
 include_once('connexionBDD.php');
-if(isset($_POST['formPlat']))
+if(isset($_POST["formPlat"]))
 {
-    //Récupérer les données
-    if (!empty($_POST["nomPlat"] && $_POST["urlImage"] && $_POST["description"] && $_POST["quantiteDispo"] && $_POST["prixUnitaire"]))
-    {
-          $nomPlat = htmlspecialchars($_POST["nomPlat"]);
-          $urlImage = htmlspecialchars($_POST["urlImage"]);
-          $description = $_POST["description"];
-          $quantiteDispo = $_POST["quantiteDispo"];
-          $prixUnitaire = $_POST["prixUnitaire"];
-          $id = '\N'; /* auto-increment */
+      if (!empty($_POST["nomPlat"] && $_POST["urlImage"] && $_POST["description"] && $_POST["quantiteDispo"] && $_POST["prixUnitaire"]))
+      {
+        //Récupérer les données
+        $nomPlat = htmlspecialchars($_POST["nomPlat"]);
+        $urlImage = htmlspecialchars($_POST["urlImage"]);
+        $description = $_POST["description"];
+        $quantiteDispo = $_POST["quantiteDispo"];
+        $prixUnitaire = $_POST["prixUnitaire"];
+            //verification: nombre de caractère saisie correspond au nbre de caractère paramétré en base de données
+            $nomPlatLength=strlen($nomPlat);
+            if($nomPlatLength <=150)
+            {
+              //verification: nombre de caractère saisie correspond au nbre de caractère paramétré en base de données
+                $urlImageLength=strlen($urlImage);
+                if($urlImageLength<=150)
+                {
+                  //verification: nombre de caractère saisie correspond au nbre de caractère paramétré en base de données
+                    $descriptionLength=strlen($description);
+                    if($descriptionLength<=250)
+                    {
+                      //verification: nombre de quantité saisie de caractère saisie correspond au nbre de caractère paramétré en base de données
+                          $quantiteDispoLenght=strlen($quantiteDispo);
+                          if($quantiteDispoLenght<=5){
+                            $prixUnitaireLenght=strlen($prixUnitaire);
+                            if($prixUnitaireLenght<=5){
+                              if($prixUnitaire>0){
+                                  if($quantiteDispo>0){
 
-          //verification du nombre de caractère saisie
-          $nomPlatLength=strlen($nomPlat);
-          if($nomPlatLength <=150)
-          {
-              $urlImageLength=strlen($urlImage);
-              if($urlImageLength<=150)
-              {
-                  $descriptionLength=strlen($description);
-                  if($descriptionLength<=250)
-                  {
-                  	  $query = "INSERT INTO Plat(Id, nomPlat, urlImage, description, quantiteDispo, prixUnitaire) VALUES ('$id','$nomPlat','$urlImage','$description', '$quantiteDispo', '$prixUnitaire')";
-                      $result = mysqli_query($mysqli,$query);
-                      if (!$result)
-                      {
-                          echo "<p>Désolée, requête impossible ! </p>";
-                      }
-                      else
-                      {
-                          $nomPlat = mysqli_fetch_assoc($result);
-                          $success ='<p style="text-align:center; color:green"> Votre plat'.$nomPlat['nomPlat'].' a été bien ajouté !</p>';
-                      }
-                      $mysqli->close();
-                  }
-                  else
-                  {
-                      $erreur='<p style="text-align:center; color:red">Votre description dépasse 250 caractères!<p>';
-                  }
+                                      $query = "INSERT INTO plat(nomPlat, urlImage, description, quantiteDispo, prixUnitaire)
+                                                        VALUES ('$nomPlat','$urlImage','$description', '$quantiteDispo', '$prixUnitaire');";
+                                      $result = mysqli_query($mysqli,$query);
+                                      if ($result)
+                                      {
+                                        $query="SELECT * FROM plat where nomPlat='$nomPlat';";
+                                        $result=mysqli_query($mysqli,$query);
+                                        if($result)
+                                        {
+                                          while($NomPlat = mysqli_fetch_assoc($result))
+                                          {
+                                              $success='<p style="text-align:center; color:green"> '.$NomPlat['nomPlat'].'a été ajouté !</p>';
+                                          }
+                                        }
 
-              }
-              else
-              {
-                  $erreur='<p style="text-align:center; color:red">Votre urlImage dépasse 150 caractères!<p>';
-              }
+                                      }
+                                      else
+                                      {
+                                          $echec ='<p style="text-align:center; color:red">Votre plat n\'a pas pu être ajouté à la liste de plat disponible, veuillez rééssayer !</p>';
+                                      }
+                                      $mysqli->close();
+                                  }else{
+                                    $erreur ='<p style="text-align:center; color:red"> Veuillez entrer un nombre positif dans le champs quantité disponible!';
+                                  }
+                                }else{
+                                  $erreur ='<p style="text-align:center; color:red"> Veuillez entrer un nombre positif dans le champs prix unitaire!';
+                                }
+                            }else{
+                              $erreur ='<p style="text-align:center; color:red"> Veuillez entrer un nombre avec 5 chiffres dans le champs prix unitaire!';
+                            }
 
-          }
-          else
-          {
-              $erreur='<p style="text-align:center; color:red">Le nom du plat dépasse 150 caractères !<p>';
-          }
-    }
-    else
-    {
-         $erreur='<p style="text-align:center; color:red">L\'un des champs n\'a pas été complété !<p>';
-    }
+                          }else{
+                            $erreur ='<p style="text-align:center; color:red"> Veuillez entrer un nombre avec 5 chiffres !';
+                          }
+                    }
+                    else
+                    {
+                        $erreur='<p style="text-align:center; color:red">Votre description dépasse 250 caractères!<p>';
+                    }
+
+                }
+                else
+                {
+                    $erreur='<p style="text-align:center; color:red">Votre urlImage dépasse 150 caractères!<p>';
+                }
+
+            }
+            else
+            {
+                $erreur='<p style="text-align:center; color:red">Veuillez saisir un nom de plat ayant moins de 150 caractères !<p>';
+            }
+      }
+      else
+      {
+           $erreur='<p style="text-align:center; color:red">L\'un des champs n\'a pas été complété !<p>';
+      }
 }
 
 ?>
-
 <!DOCTYPE html>
 <html>
  <head>
      <meta charset="UTF-8" />
      <meta name="author" content="Manuele" />
-    <title> Formulaire</title>
-    <style type="text/css">
-          label { display: inline-block;
-                     width : 200px;
-                     text-align : left;
-                     font-weight: bold;
-                     margin-left: 5px;
-                    }
-    </style>
- </head>
+    <title> Ajouter un plat</title>
+</head>
 <body>
     <h1> Formulaire Plat </h1>
+    <div>
     <?php
     if (isset($erreur)) { echo $erreur; }
     if (isset($success)) { echo $success; }
     ?>
+  </div>
     <form name="formPlat" action="" method="POST">
 
                 <label>Nom</label>
@@ -98,14 +121,14 @@ if(isset($_POST['formPlat']))
                 <br/>
 
                 <label>Quantité disponible </label>
-                <input type="text" id="quantiteDispo" name="quantiteDispo" placeholder="Saisissez la quantité disponible de votre plat" size="40" maxlength="5" />
+                <input type="number" id="quantiteDispo" name="quantiteDispo" placeholder="Saisissez la quantité disponible de votre plat" size="40" maxlength="6" />
                 <br/>
 
                 <label>Prix unitaire </label>
-                <input type="text" id="prixUnitaire" name="prixUnitaire" placeholder="Saisissez le prix unitaire de votre plat" size="40" maxlength="5" />
+                <input type="number" id="prixUnitaire" name="prixUnitaire" placeholder="Saisissez le prix unitaire de votre plat" size="40" maxlength="5" />
                 <br/>
 
-                <input type="submit" value="Ajouter votre plat" class="bouton" />
+                <input name="formPlat" type="submit" value="Ajouter votre plat" class="bouton" />
 
     </form>
 </body>
