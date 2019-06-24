5f43a13b16f6f490d970769ac91915cfc2e580aa
LU <yiying.lu@outlook.com>
2019-06-25 00:08:35 +0200

    Bouton Supprimer -> afficher message erreur avec le nom du plat -> suppression de la BDD ne fonctionne pas bien ...

--- SupprimerPlat.php
+++ SupprimerPlat.php
@@ -23,21 +23,60 @@ include_once('connexionBDD.php');
     <a href="home.php"><input class="bouton" type="button" value="Revenir en arrière"> </a>
   </div>
   <?php
-    //recuperer données recettes
+    //recuperer données plats
   $sql = "SELECT * FROM Plat" ;
   $resultat = $mysqli->query($sql);
 
-
   echo "<div id='conteneur'>";
-  while ($ligne = $resultat->fetch_assoc()) {
-    echo  "<div class='recette'>
-    <img class='illustration' src='Images/" . $ligne['urlImage']."' >
+  while ($ligne = $resultat->fetch_assoc())
+  {
+    echo "<form name='formSup".$ligne['Id']."' action='' method='POST'>";
+    echo
+    "<div class='recette'>
+    <img class='illustration' src='Images/" . $ligne['urlImage']."'>
     <div class='nom'>" . $ligne['nomPlat'] ."</div>
     <div class='description'>" . $ligne['description'] ."</div>
-    <div id='ajout' ><input class='bouton' type='submit' value='Supprimer le plat' ></div>
+    <div id='ajout' ><input name='".$ligne['Id']."'class='bouton' type='submit' value='Supprimer le plat ".$ligne['Id']."'></div>
     </div>";
+    echo "</form>";
+    $id = $ligne['Id'];
   }
   echo "</div>";
+
+    //Création d'un tableau ID
+  for ($i=0; $i<=$id; $i++)
+  {
+    $sup = $i;
+    //Si utilisateur clique sur bouton ID
+    if (isset ($_POST[$sup]))
+    {
+      echo $sup;
+      $query = "SELECT nomPlat FROM Plat WHERE Id=$sup";
+      $result = mysqli_query($mysqli,$query);
+      if ($result)
+      {
+        while($ligne = $result->fetch_assoc())
+        {
+          $nomplat = $ligne ['nomPlat'];
+        }
+          echo "<div id='boutons'>";
+            echo "<form name='Confirmation' action='' method='POST'>";
+            echo "<p style='color:red'> Êtes-vous sûr de vouloir supprimer le plat : ".$nomplat." ? </p>
+                  <input name='Confirmation' type='submit' value='Confirmer' class='bouton'>";
+            echo "</form>";
+          echo "</div>";
+
+
+          if (isset($_POST["Confirmation"]))
+          {
+            echo "ok";
+            $query2 = "DELETE FROM Plat WHERE Id=$sup";
+            $result2 = mysqli_query($mysqli,$query2);
+            header("Refresh: 0;url=SupprimerPlat.php");
+          }
+      }
+    }
+  }
 
   $mysqli->close();
   ?>
