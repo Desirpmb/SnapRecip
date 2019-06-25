ed2d4bea6a80ab58ecf7290387eb7fc053e6fef8
LU <yiying.lu@outlook.com>
2019-06-25 18:04:18 +0200

    Fichier traitementAjourPanier

---
+++ traitementAjoutPanier.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+include_once('connexionBDD.php');
+//$idPlatSupp=$_GET["varID"];
+//$query = "DELETE FROM Plat WHERE Id=$idPlatSupp";
+//$result = mysqli_query($mysqli,$query);
+header("Refresh: 0;url=SupprimerPlat.php");
+?>
