35068a012b91fb35a8b7529263e47b1a6e9c5647
LU <yiying.lu@outlook.com>
2019-06-25 18:29:06 +0200

    Update : choses à faire

--- traitementAjoutPanier.php
+++ traitementAjoutPanier.php
@@ -5,4 +5,10 @@ include_once('connexionBDD.php');
 //$query = "DELETE FROM Plat WHERE Id=$idPlatSupp";
 //$result = mysqli_query($mysqli,$query);
 header("Refresh: 0;url=SupprimerPlat.php");
+//recupérer ID, quantiteDIspo Plat
+//teste de quantite quantiteDIspo
+//inserer dans ligne commeande le Plat
+//soustrait la quantite que l'on souhaite ajouter à la quantiteDisponible
+//PrixTotal du panier, additionner tous les produit présent dans la table ligneCommande
+//presentation du oanier sous forme de tableau
 ?>
